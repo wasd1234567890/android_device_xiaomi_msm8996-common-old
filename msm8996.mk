d9fc8c3330aea7bc5468943e7c09f3587492e9dc
xNombre <kartapolska@gmail.com>
2020-07-13 22:24:58 +0200

    msm8996: Remove ancient copybit package
    It wasn't present in build anyways...
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Change-Id: Iab9e0f75669c48ca81c4be578764ca598cac25a7

--- msm8996.mk
+++ msm8996.mk
@@ -174,7 +174,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    copybit.msm8996 \
     gralloc.msm8996 \
     hwcomposer.msm8996 \
     memtrack.msm8996 \

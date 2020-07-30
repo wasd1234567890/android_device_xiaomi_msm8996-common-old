63d91c5686b14a952dd7825acc4753c3dd8bce6c
xNombre <kartapolska@gmail.com>
2020-07-30 14:00:13 +0200

    props: Remove unused debug.sf.enable_gl_backpressure
    This is nonexistent in CAF; debug.sf.disable_backpressure makes debug.sf.enable_gl_backpressure noop.
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Change-Id: Ieb9541f4e53855eda17ea05db0cedd431ab8694b

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -108,7 +108,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.latch_unsignaled=1 \
     debug.sf.recomputecrop=0 \
     debug.sf.disable_backpressure=1 \
-    debug.sf.enable_gl_backpressure=1 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3

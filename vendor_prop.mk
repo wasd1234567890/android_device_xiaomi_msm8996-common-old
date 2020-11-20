ac5b268e53e299dd32e9fd40181e884737f09be4
xNombre <kartapolska@gmail.com>
2020-11-20 23:55:55 +0100

    prop: dalvik speed tweaks
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -76,6 +76,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-filter=speed \
+    dalvik.vm.image-dex2oat-filter=speed \
+    dalvik.vm.dex2oat64.enabled=true \
     dalvik.vm.heapgrowthlimit=192m \
     dalvik.vm.heapstartsize=8m \
     dalvik.vm.heapsize=512m

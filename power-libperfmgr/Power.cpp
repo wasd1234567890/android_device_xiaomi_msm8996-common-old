931db2a74f4eecb5019b1be4a2c30586a8a5cc1d
xNombre <kartapolska@gmail.com>
2020-12-05 13:50:27 +0100

    power-libperfmgr: Fix constant power hal crashes
    In commit 42c9748f84c7792ac96b34c9e39f29425f89e700 I copied wrong line from upstream hal which caused Power HAL to crash constantly.
    
    Fix this OP mistake by completely removing unhelpful errors, PowerStats arent utilized on user/userdebug builds anyways.
    
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- power-libperfmgr/Power.cpp
+++ power-libperfmgr/Power.cpp
@@ -207,14 +207,12 @@ Return<void> Power::setFeature(Feature feature, bool activate) {
 }
 
 Return<void> Power::getPlatformLowPowerStats(getPlatformLowPowerStats_cb _hidl_cb) {
-    LOG(FATAL) << "Invalid config: " << kPowerHalConfigPath;
     _hidl_cb({}, Status::SUCCESS);
     return Void();
 }
 
 // Methods from ::android::hardware::power::V1_1::IPower follow.
 Return<void> Power::getSubsystemLowPowerStats(getSubsystemLowPowerStats_cb _hidl_cb) {
-    LOG(ERROR) << "getSubsystemLowPowerStats not supported. Use IPowerStats HAL.";
     _hidl_cb({}, Status::SUCCESS);
     return Void();
 }

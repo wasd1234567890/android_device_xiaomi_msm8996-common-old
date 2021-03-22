17c33941c34cd9a3b7dc2df24c6a58f57df065af
xNombre <kartapolska@gmail.com>
2021-03-22 12:22:30 +0100

    Revert "msm8996: Enabel ro.hwui.render_ahead"
    Reverted in commit https://github.com/LineageOS/android_device_google_coral/commit/07d7977224bdf2e32d711025ed35a3956fc7ef54
    
    This reverts commit 01a0865b96a8784143abbd7ccd04a76cb8f70f85.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -125,10 +125,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.enable_default_color_mode=1 \
     vendor.gralloc.enable_fb_ubwc=1
 
-# HWUI
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.render_ahead=1
-
 # Surfaceflinger
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=480 \

8bd31b968efaf2b3fc60021f544440b5a4224ad0
xNombre <kartapolska@gmail.com>
2021-03-29 21:55:23 +0200

    msm8996: Fix missing ims_media_jni dependency
    lib-imsvt.so needs 64-bit version of android.hardware.camera.provider@2.4.so
    
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -137,7 +137,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl:32 \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.common@1.0 \
     camera.msm8996 \

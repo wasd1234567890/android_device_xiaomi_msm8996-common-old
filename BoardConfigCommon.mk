db931904f6e04f987037e16cae2fae64174e2c33
Nicholas Magill <nmagill1979@gmail.com>
2021-08-06 18:35:39 +0100

    msm8996-common: Enable FBE

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -105,9 +105,6 @@ TARGET_USES_COLOR_METADATA := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
-# Encryption
-TARGET_HW_DISK_ENCRYPTION := true
-
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(VENDOR_PATH)/config.fs
 

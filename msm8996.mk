738e61e54081a4dc01a8e7c4c9e04b55a607f604
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    msm8996-common: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- msm8996.mk
+++ msm8996.mk
@@ -213,7 +213,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # For config.fs
 PRODUCT_PACKAGES += \
